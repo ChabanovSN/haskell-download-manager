b244a9c28bb8a90ff3f09947998a35cd86e5308f
Wael Ben Dhia <wael@localhost.localdomain>
2018-01-08 01:24:01 +0100

    Minor refactoring Added add download button

--- src/HDMWindow.hs
+++ src/HDMWindow.hs
@@ -8,39 +8,54 @@ import Graphics.UI.Gtk
 import Settings
 
 makeWindow :: IO Window
-makeWindow =do
-  w <-windowNew 
-  set w [windowTitle := "Download Manager", windowResizable := True]
+makeWindow = do
+  w <- window
   box <- vBoxNew False 0
-  _ <-
-    on w deleteEvent $ do
-      liftIO mainQuit
-      return False
-  makeMenu >>= \m->boxPackStart box m PackNatural 0
-  sWindow <- scrolledWindowNew Nothing Nothing
-  boxPackStart box sWindow PackGrow 0
-  vBoxNew False 0 >>= containerAdd sWindow 
-  -- mapM_
-  --   (\dl -> do 
-  --      dl2 <- dl
-  --      boxPackStart downloadContainer dl2 PackNatural 0)
-  --   testShow
+  makeMenu >>= \m -> boxPackStart box m PackNatural 0
+  scroller >>= \s -> boxPackStart box s PackGrow 0
   containerAdd w box
   return w
+  where
+    window = do
+      w <- windowNew
+      set w [windowTitle := "Download Manager", windowResizable := True]
+      on w deleteEvent $ do
+        liftIO mainQuit
+        return False
+      return w
+    scroller = do
+      w <- scrolledWindowNew Nothing Nothing
+      vBoxNew False 0 >>= containerAdd w
+      return w
 
 makeMenu :: IO MenuBar
-makeMenu =
-  menuBarNew >>= \m -> do
-    makeFileButton >>= menuShellAppend m
-    menuItemNewWithLabel "Edit" >>= menuShellAppend m
-    return m
+makeMenu = do
+  m <- menuBarNew
+  makeFileButton >>= menuShellAppend m
+  makeEditButton >>= menuShellAppend m
+  return m
+
+makeEditButton :: IO MenuItem
+makeEditButton = do
+  edit <- menuItemNewWithLabel "Edit"
+  subMenu >>= menuItemSetSubmenu edit
+  return edit
+  where
+    subMenu = do
+      sub <- menuNew
+      addFile <- menuItemNewWithLabel "New Download"
+      menuAttach sub addFile 0 1 0 1
+      return sub
 
 makeFileButton :: IO MenuItem
 makeFileButton = do
-  files<-menuItemNewWithLabel "File"
-  sub<-menuNew
-  menuItemSetSubmenu files sub
-  settings <-menuItemNewWithLabel "Settings"
-  _ <- on settings menuItemActivated $ settingsWindow >>= widgetShowAll
-  menuAttach sub settings 0 1 0 1
+  files <- menuItemNewWithLabel "File"
+  subMenu >>= menuItemSetSubmenu files
   return files
+  where
+    subMenu = do
+      sub <- menuNew
+      settings <- menuItemNewWithLabel "Settings"
+      on settings menuItemActivated $ settingsWindow >>= widgetShowAll
+      menuAttach sub settings 0 1 0 1
+      return sub
--- src/Settings.hs
+++ src/Settings.hs
@@ -16,23 +16,27 @@ newtype HDMSettings = HDMSettings
 
 settingsWindow :: IO Window
 settingsWindow = do
-  w <- windowNew
-  state <- getSettings "./settings" >>= newIORef
-  set w
-    [ windowTitle := "Settings"
-    , windowResizable := False
-    , windowModal := True
-    , windowDestroyWithParent := True
-    ]
-  -- _ <-
-  --   after w deleteEvent $ do
-  --     liftIO $ saveSettings state
-  --     return False
+  w <- window
+  state <- getState
   vb <- vBoxNew False 16
   downloadDirectoryChooser state >>= \dc -> boxPackStart vb dc PackNatural 8
-  buttons <-
-    hButtonBoxNew >>= \bBox -> do
-      buttonBoxSetLayout bBox ButtonboxEnd
+  buttonsBox state >>= \buttons -> boxPackEnd vb buttons PackNatural 8
+  containerAdd w vb
+  return w
+  where
+    window = do
+      w <- windowNew
+      set
+        w
+        [ windowTitle := "Settings"
+        , windowResizable := False
+        , windowModal := True
+        , windowDestroyWithParent := True
+        ]
+      return w
+    getState = getSettings "./settings" >>= newIORef
+    buttonsBox state = do
+      bBox <- hButtonBoxNew
       confirm <- buttonNewWithLabel "Confirm"
       _ <-
         on confirm buttonActivated $ do
@@ -40,9 +44,6 @@ settingsWindow = do
           return ()
       boxPackEnd bBox confirm PackNatural 16
       return bBox
-  boxPackEnd vb buttons PackNatural 8
-  containerAdd w vb
-  return w
 
 saveSettings :: IORef HDMSettings -> IO ()
 saveSettings state = readIORef state >>= (`writeSettingsToFile` "./settings")
@@ -50,32 +51,36 @@ saveSettings state = readIORef state >>= (`writeSettingsToFile` "./settings")
 downloadDirectoryChooser :: IORef HDMSettings -> IO Table
 downloadDirectoryChooser state = do
   t <- tableNew 1 2 False
-  let attach col child = tableAttach t child col (col + 1) 0 1 [] [] 16 16
-  labelNew (Just "Download directory: ") >>= attach 0
-  fb <- fileChooserButtonNew "Download directory" FileChooserActionSelectFolder
-  _ <- readIORef state >>= fileChooserSetFilename fb . downloadDirectory
-  _ <-
-    on fb fileChooserButtonFileSet $
-    fileChooserGetFilename fb >>= \fp ->
-      case fp of
-        Just path -> do
-          _ <-
-            atomicModifyIORef state $ \_ ->
-              let r = HDMSettings path
-              in (r, r)
-          return ()
-        Nothing -> return ()
-  attach 1 fb
+  labelNew (Just "Download directory: ") >>= attach t 0
+  fileButton >>= attach t 1
   return t
+  where
+    fileButton = do
+      fb <-
+        fileChooserButtonNew
+          "Download directory"
+          FileChooserActionSelectFolder
+      readIORef state >>= fileChooserSetFilename fb . downloadDirectory
+      on fb fileChooserButtonFileSet $
+        fileChooserGetFilename fb >>= \fp -> do
+          atomicModifyIORef state $ \cur ->
+            case fp of
+              Just path ->
+                let r = HDMSettings path
+                in (r, r)
+              _ -> (cur, cur)
+          return ()
+      return fb
+    attach t col child = tableAttach t child col (col + 1) 0 1 [] [] 16 16 
 
 getSettings :: String -> IO HDMSettings
 getSettings filePath = do
   res <- try (readFile filePath) :: IO (Either IOError String)
   return $
-    toHDMSettings
-      (case res of
-         Left _ -> ""
-         Right path -> path)
+    toHDMSettings $
+    case res of
+      Left _ -> ""
+      Right path -> path
 
 toHDMSettings :: String -> HDMSettings
 toHDMSettings a = HDMSettings {downloadDirectory = extract "dir" params}
@@ -85,10 +90,10 @@ toHDMSettings a = HDMSettings {downloadDirectory = extract "dir" params}
       case filter (\d -> fst d == name) groups of
         [] -> ""
         l -> snd $ head l
+    params = map toPair [splitOn "::" a]
     toPair l
       | length l <= 1 = ("", "")
       | otherwise = (head l, l !! 1)
-    params = [toPair t | t <- [splitOn "::" a]]
 
 writeSettingsToFile :: HDMSettings -> FilePath -> IO ()
 writeSettingsToFile settings filePath =
